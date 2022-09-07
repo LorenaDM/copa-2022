d15d59b378619d539cc749c18a5f387b2558e182
FlutLab.io <>
2022-09-07 18:23:24 +0000

    Trabalho-Fausto

--- lib/ui/lista_equipes.dart
+++ lib/ui/lista_equipes.dart
@@ -1,35 +1,47 @@
-import'package:flutter/material.dart;
-import'ui/lista_equipes.dart';
-
-void 
+import 'package:flutter/material.dart';
 
 class ListaEquipes extends StatelessWidget {
-  
-  const ListaEquipes({Key? key}): super(key: key);
+  ListaEquipes({Key? key}) : super(key: key);
 
   final equipes = [
-    {"nome": "Catar",
-     "grupo": "A",
-     "url_simbolo": "https://upload.wikimedia.org/wikipedia/pt/a/a9/Associa%C3%A7%C3%A3o_do_Qatar_de_Futebol.png"
+    {
+      "nome": "Catar",
+      "grupo": "A",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/a/a9/Associa%C3%A7%C3%A3o_do_Qatar_de_Futebol.png",
     },
-    {"nome": "Equador",
-     "grupo": "A",
-     "url_simbolo": "https://upload.wikimedia.org/wikipedia/pt/thumb/7/74/FEFecu.png/150px-FEFecu.png"
+    {
+      "nome": "Equador",
+      "grupo": "A",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/7/74/FEFecu.png",
     },
-    {"nome": "Senegal",
-     "grupo": "A",
-     "url_simbolo": "https://upload.wikimedia.org/wikipedia/pt/thumb/7/7c/FSenegalaiseF.png/150px-FSenegalaiseF.png"
-    }
-  ]
-  @override 
+    {
+      "nome": "Paises Baixos",
+      "grupo": "A",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/f/fe/Royal_Netherlands_Football_Association_Logo.svg.png",
+    },
+    {
+      "nome": "Senegal",
+      "grupo": "A",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/7/7c/FSenegalaiseF.png",
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: equipes.length,
         itemBuilder: (context, i) {
           var equipe = equipes[i];
           var simbolo = Image.network(equipe["url_simbolo"]!);
-          return ListTitle
-        },
-    );
+          return ListTile(
+            leading: simbolo,
+            title: Text(equipe["nome"]!),
+            subtitle: Text("Grupo " + equipe["grupo"]!),
+          );
+        });
   }
 }
